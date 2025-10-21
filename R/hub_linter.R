fe1dc3e62cd4559843ba0cf04515aa994545bfc0
FJeronimo42 <65516057+FJeronimo42@users.noreply.github.com>
2025-10-21 11:14:51 -0300

    v.1.0
    Upload:
    Script 1 - Manipulação de Dados

---
+++ R/hub_linter.R
@@ -0,0 +1,22 @@
+# Diretório
+dir.create('Data')
+dir.create('R')
+dir.create('Figures')
+
+# Biblioteca
+library('tidyverse')
+
+# Dados
+data(iris)
+
+str(iris)
+
+# Manipulação
+iris_data <- iris %>% 
+  select(Species, Sepal.Length, Sepal.Width) %>% # Responsabilidade do Eduardo
+  filter(Species != 'setosa') %>% # Responsabilidade do Bruno
+  mutate(Sepal.Rate = Sepal.Length/Sepal.Width) %>% # Responsabilidada da Rafaela
+  group_by(Species) %>% 
+  summarise(Sepal.Rate.Mean = mean(Sepal.Rate)) %>% # Responsabilidade da Heloisa
+  glimpse()
+  
